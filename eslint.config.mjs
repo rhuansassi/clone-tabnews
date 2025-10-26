b1808b84d541be774965564f0d0fe068210fd88b
Rhuan Sassi <rhuansassi@gmail.com>
2025-10-26 19:58:52 -0300

    add and configure `eslint`

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig(
+  [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      languageOptions: {
+        globals: { ...globals.browser, ...globals.node, ...globals.jest },
+      },
+    },
+    pluginReact.configs.flat.recommended,
+    {
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+    },
+  ],
+  [globalIgnores(["node_modules/", ".next/"])],
+);
